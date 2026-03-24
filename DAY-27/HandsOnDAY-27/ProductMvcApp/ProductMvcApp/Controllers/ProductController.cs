714b15fa2157c3cbc51a1139aa49b85e576ebfec
keerthivjr <keerthivasankeerthi43@gmail.com>
2026-03-24 21:17:38 +0530

    WEEK-6 DAY-27 HandsOn

---
+++ DAY-27/HandsOnDAY-27/ProductMvcApp/ProductMvcApp/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductMvcApp.Models;
+using System.Collections.Generic;
+
+namespace ProductMvcApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            // Dummy data (no database)
+            List<Product> products = new List<Product>
+            {
+                new Product { Id = 1, Name = "Laptop", Price = 50000 },
+                new Product { Id = 2, Name = "Mobile", Price = 20000 },
+                new Product { Id = 3, Name = "Headphones", Price = 3000 }
+            };
+
+            return View(products);
+        }
+    }
+}
