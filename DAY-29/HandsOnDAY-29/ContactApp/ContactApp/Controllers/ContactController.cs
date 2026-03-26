9632b87aa4c094ce13aa11d7e40ce7d7f45d2ff6
keerthivjr <keerthivasankeerthi43@gmail.com>
2026-03-26 21:21:59 +0530

    WEEK-6 Added DAY-28,29 HandsOn

---
+++ DAY-29/HandsOnDAY-29/ContactApp/ContactApp/Controllers/ContactController.cs
@@ -0,0 +1,58 @@
+﻿using ContactApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactApp.Controllers
+{
+    public class ContactController : Controller
+    {
+        // In memory list
+        private static List<ContactInfo> contacts = new List<ContactInfo>
+        {
+            new ContactInfo { ContactId = 1, FirstName = "Sakshi", LastName = "Jadhav", CompanyName = "Cognizant", EmailId = "sakshi@gmail.com", MobileNo = 8765463534, Designation = "Software Engineer" },
+            new ContactInfo { ContactId = 2, FirstName = "Mahi", LastName = "Reddy", CompanyName = "Capgemini", EmailId = "mahi@gmail.com", MobileNo = 9876764337, Designation = "Frontend Developer" }
+        };
+
+        // SHOW ALL CONTACTS
+        public ActionResult ShowContacts()
+        {
+            return View(contacts);
+        }
+
+        // GET CONTACT BY ID
+        public ActionResult GetContactById(int id)
+        {
+            var contact = contacts.FirstOrDefault(c => c.ContactId == id);
+            if (contact == null)
+            {
+                return Content("Contact Not found!");
+            }
+            return View(contact);
+        }
+
+        // LOAD ADD CONTACT PAGE
+        public ActionResult AddContact()
+        {
+            return View();
+        }
+
+        // SAVE CONTACT
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AddContact(ContactInfo contact)
+        {
+            if (ModelState.IsValid)
+            {
+                // Check if ID already exists (since we don't have a DB to handle unique keys)
+                if (contacts.Any(c => c.ContactId == contact.ContactId))
+                {
+                    ModelState.AddModelError("ContactId", "This ID is already taken!");
+                    return View(contact);
+                }
+
+                contacts.Add(contact);
+                return RedirectToAction("ShowContacts");
+            }
+            return View(contact);
+        }
+    }
+}
---
+++ DAY-29/HandsOnDAY-29/ContactApp/ContactApp/Models/ContactInfo.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContactApp.Models
+{
+    public class ContactInfo
+    {
+        [Required(ErrorMessage = "Contact ID is required")]
+        public int ContactId { get; set; }
+
+        [Required(ErrorMessage = "First Name cannot be empty")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required")]
+        public string LastName { get; set; }
+
+        public string CompanyName { get; set; } // Optional field
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string EmailId { get; set; }
+
+        [Required(ErrorMessage = "Mobile Number is required")]
+        public long MobileNo { get; set; }
+
+        public string Designation { get; set; }
+    }
+}
