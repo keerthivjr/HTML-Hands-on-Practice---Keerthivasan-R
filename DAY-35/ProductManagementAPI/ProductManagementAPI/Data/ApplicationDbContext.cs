7993bc41ecf42aeef70cab83a6774839a89b7823
keerthivjr <keerthivasankeerthi43@gmail.com>
2026-04-19 22:41:21 +0530

    Ended WEEK-7 Added DAY-35 Hands on

---
+++ DAY-35/ProductManagementAPI/ProductManagementAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductManagementAPI.Models;
+
+namespace ProductManagementAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        // This method is where you configure the model details
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Explicitly set the precision and scale for the Price property
+            modelBuilder.Entity<Product>()
+                .Property(p => p.Price)
+                .HasPrecision(18, 2);
+        }
+    }
+}
---
+++ DAY-35/ProductManagementAPI/ProductManagementAPI/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProductManagementAPI.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+
+        public string Category { get; set; } = string.Empty;
+        public int StockAvailable { get; set; }
+    }
+}
