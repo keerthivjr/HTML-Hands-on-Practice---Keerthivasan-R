400773dd76558d88cf174d4a5d14482af4bfa50d
keerthivjr <keerthivasankeerthi43@gmail.com>
2026-04-19 23:01:12 +0530

    Ended WEEK-8 Added DAY-40 HandsOn

---
+++ DAY-40/EmployeeManagementSystem/EmployeeManagement.API/Services/EmployeeService.cs
@@ -0,0 +1,11 @@
+﻿namespace EmployeeManagement.API.Services;
+public class EmployeeService
+{
+    public double CalculateSalary(double basicSalary)
+    {
+        if (basicSalary < 0)
+            throw new ArgumentException("Salary cannot be negative");
+
+        return basicSalary + (0.2 * basicSalary);
+    }
+}
---
+++ DAY-40/EmployeeManagementSystem/EmployeeManagement.Tests/EmployeeServiceTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using EmployeeManagement.API.Services;
+
+[TestFixture]
+public class EmployeeServiceTests
+{
+    private EmployeeService _service;
+
+    [SetUp]
+    public void Setup()
+    {
+        _service = new EmployeeService();
+    }
+
+    [Test]
+    public void CalculateSalary_ValidInput_ReturnsCorrectSalary()
+    {
+        var result = _service.CalculateSalary(10000);
+        Assert.AreEqual(12000, result);
+    }
+
+    [Test]
+    public void CalculateSalary_NegativeInput_ThrowsException()
+    {
+        Assert.Throws<ArgumentException>(() => _service.CalculateSalary(-5000));
+    }
+}
